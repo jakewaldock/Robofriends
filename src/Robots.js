dbd6b4fc3ba5bbbfb79a9bdc3f3c72f3388e6265
jakewaldock <50667312+jakewaldock@users.noreply.github.com>
2019-07-25 11:54:51 +0100

    Adding Project

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Jolty Jake',
+    username: 'Jake',
+    email: 'Joltyjake@gmail.uk'
+  },
+  {
+    id: 2,
+    name: 'Lumpy Lesley',
+    username: 'Lesley',
+    email: 'Lumpylesley@virgin.com'
+  },
+  {
+    id: 3,
+    name: 'Naughty Neale',
+    username: 'Neale',
+    email: 'Naughtyneale@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Voluptuous Val',
+    username: 'Val',
+    email: 'Voluptuousval@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Artificial Alfie',
+    username: 'Alfie',
+    email: 'Artificialalfie@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Tectonic Tia',
+    username: 'Tia',
+    email: 'Tectonictia@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Loud Liam',
+    username: 'Liam',
+    email: 'Loudliam@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Juggernaut Jackie',
+    username: 'Jackie',
+    email: 'Juggernautjackie@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Supreme Sirius',
+    username: 'Sirius',
+    email: 'Supremesirius@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Mortal Max',
+    username: 'Max',
+    email: 'Mortalmax@karina.biz'
+  }
+];
